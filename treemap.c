bb46eec8bd037cc48401d3a089eecbecb9c03d82
Replit user <>
2023-04-21 17:58:22 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -45,13 +45,8 @@ El siguiente código muestra como inicializar la función de comparación. Reser
   nuevo -> current = NULL;
   nuevo->lower_than = lt;
   return nuevo;
-    }
-
-TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
-
-    //new->lower_than = lower_than;
-    return NULL;
 }
+
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
